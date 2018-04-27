5c036c33c38238a32d5e95cc9807bc66e5d1aaf9
alex.baloc <alex.baloc@gmail.com>
2018-04-27 16:40:26 +0300

    WIP changes to support external network & version

--- packages/fabric-dev-servers/fabric-scripts/hlfv11/downloadFabric.sh
+++ packages/fabric-dev-servers/fabric-scripts/hlfv11/downloadFabric.sh
@@ -5,13 +5,16 @@ set -e
 
 # Set ARCH
 ARCH=`uname -m`
+VERSION=${HL_FABRIC_TAG:=1.1.0}
 
 # Grab the current directory.
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+echo "checking for version ${VERSION}"
+
 # Pull and tag the latest Hyperledger Fabric base image.
-docker pull hyperledger/fabric-peer:$ARCH-1.1.0
-docker pull hyperledger/fabric-ca:$ARCH-1.1.0
-docker pull hyperledger/fabric-ccenv:$ARCH-1.1.0
-docker pull hyperledger/fabric-orderer:$ARCH-1.1.0
+docker pull hyperledger/fabric-peer:$ARCH-$VERSION
+docker pull hyperledger/fabric-ca:$ARCH-$VERSION
+docker pull hyperledger/fabric-ccenv:$ARCH-$VERSION
+docker pull hyperledger/fabric-orderer:$ARCH-$VERSION
 docker pull hyperledger/fabric-couchdb:$ARCH-0.4.6
