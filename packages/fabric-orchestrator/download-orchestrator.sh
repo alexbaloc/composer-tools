213dc1ee4f7b053f23886d1b8ef2d2d056bca47e
alex.baloc <alex.baloc@gmail.com>
2018-05-01 18:00:25 +0300

    fix

--- packages/fabric-orchestrator/download-orchestrator.sh
+++ packages/fabric-orchestrator/download-orchestrator.sh
@@ -5,11 +5,11 @@ echo "Downloading orchestrator files"
 # Basic docker & scripts  
 curl -O https://raw.githubusercontent.com/alexbaloc/composer-tools/master/packages/fabric-orchestrator/Dockerfile
 curl -O https://raw.githubusercontent.com/alexbaloc/composer-tools/master/packages/fabric-orchestrator/entrypoint.sh \
-  && chmod +x entrypoint.sh
+  && chmod +x ./entrypoint.sh
 
 # Download the fabric-tools development server next to it
 curl -O https://raw.githubusercontent.com/alexbaloc/composer-tools/master/packages/fabric-orchestrator/download-fabric-tools.sh \
-  && chmod +x download-fabric-tools.sh \
-  && ./ download-fabric-tools.sh
+  && chmod +x ./download-fabric-tools.sh \
+  && ./download-fabric-tools.sh
 
 echo "Done"
